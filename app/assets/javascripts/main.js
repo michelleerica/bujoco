7907499ffc09c5641312c181e970ba8a719b95da
Michelle Erica <michelle.erica.lo@gmail.com>
2017-08-02 22:40:15 +1000

    is this working?

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -7,8 +7,7 @@ $(document).ready(function(){
 
   //------------ create canvas ------------ //
 if ($("body.designs.new").length ||
-    $("body.designs.edit").length ||
-    $("body.designs.show").length) {
+    $("body.designs.edit").length) {
   canvas = new fabric.Canvas('mainCanvas', { selection: false, backgroundColor: "#FFF4F4" });
   var grid = 25;
 
@@ -346,33 +345,5 @@ if ($("body.designs.new").length ||
 
   }
 } // new / edit
-
-  if ($("body.designs.edit").length){
-  console.log(elements);
-  console.log('length', elements.length);
-
-  for (var i = 0; i < elements.length; i++) {
-    let e = elements[i];
-    console.log('element: ', e);
-
-    var src = "http://res.cloudinary.com/michelleerica/image/upload/v1501323957/"+ e.flourish.name +".png";
-
-    var flourish = fabric.Image.fromURL(src, function(showImg){
-      // debugger;
-      showImg.setLeft(e.left); console.log('e.left', e.left);
-      showImg.setTop(e.top);
-      showImg.setAngle(e.angle);
-      showImg.setScaleX(e.scaleX);
-      showImg.setScaleY(e.scaleY);
-      showImg.id = e.flourish.id;
-
-      canvas.add(showImg);
-      console.log('ON', i);
-
-    }, {crossOrigin: 'Anonymous'})
-
-  }
-
-} //edit only
 
 }); // end of document ready
