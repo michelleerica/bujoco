d65d39aeb879a973527178ddfb8318e644659a86
Michelle Erica <michelle.erica.lo@gmail.com>
2017-08-02 22:36:46 +1000

    edit save working on new page again (no need to change page after initial save

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -98,14 +98,14 @@ if ($("body.designs.new").length ||
            .fileupload('add', { files: [ blob ] });
          });
 
-    })
+  })
     //  ------ save cloudinary data to DB ------- //
 
   var saveClData = function(image){
     var data = {
       image: image,
     };
-    debugger;
+    // debugger;
 
     if (design_id){
       data.design_id = design_id;
@@ -223,7 +223,7 @@ if ($("body.designs.new").length ||
 	});
 
   var saveElementData = function(info){
-    console.log('element info:', info);
+    console.log('IN SAVE ELEMENT DATA FUNCTION: element info:', info);
 
     var data = {
       name: $('#designName').val(),
@@ -257,12 +257,122 @@ if ($("body.designs.new").length ||
       $('#designId').text(design_id);
 
     }).fail(function(xhr, err, status) {
+      console.log(xhr, err, status);
+    });
+  };
+
+      //---------------------update and save elements to DB---------------------//
+
+        // debugger;
+
+        //rebuild existing design with element as saved in DB
+        //use elements details (e.g. img, width, height etc) hidden on design show page in flourish div and add to canvas
+      // var $imgElementShow =$('.flourish');
+      //
+      //   //flourish div created on show page as part of a loop. Number of flourish divs indicate how many elements there are to display
+
+      //------------ identify whats on canvas and update DB ------------ //
+  $("#updateDBButton").click(function(){
+    // debugger;
+    var elements = canvas.getObjects();
+    console.log(elements);
+
+    var elems = [];
+
+    for (var i = 80; i < elements.length; i++) {
+      var left = elements[i].get('left');
+      var top = elements[i].get('top');
+
+      var angle = elements[i].get('angle');
+      var scaleX = elements[i].get('scaleX');
+      var scaleY = elements[i].get('scaleY');
+      var flourish_id = elements[i].get('id');
+      console.log('scaleX', scaleX, 'scaleY', scaleY);
+      var elementInfo = {
+        left: left,
+        top: top,
+        angle: angle,
+        scaleX: scaleX,
+        scaleY: scaleY,
+        flourish_id: flourish_id
+      };
+
+      elems.push(elementInfo);
+    }
+    console.log(elems);
+    updateElementData(elems)
+
+  });
+
+  var updateElementData = function(info){
+    console.log('UPDATE ELEMENT element info:', info);
+    // debugger;
+    var data = {
+      name: $('#designName').val(),
+      elements: info,
+      design_id:designData.id
+    };
+
+    //need to get design from params
+    var url = "";
+
+    if (data.design_id !== null) {
+      url = "/designs/"+data.design_id
+    } else {
+      url = design_id
+    };
+
+    // debugger;
+
+    $.ajax({
+      url: url,
+      data: data,
+      dataType: 'json',
+      method: 'PUT'
+    }).done(function(data){
+      // debugger;
+      console.log('DATA in ajax', data);
+      $('#saveStatus').text('DB save worked');
+
+      // save design_id in a global variable,
+      // and send it with all future requests
+      // to prevent a new design being created each
+      // time we save the elements
+      // design_id = data.id;
+
+    }).fail(function(xhr, err, status) {
           console.log(xhr, err, status);
     });
 
   }
+} // new / edit
 
+  if ($("body.designs.edit").length){
+  console.log(elements);
+  console.log('length', elements.length);
 
-} //designs new
+  for (var i = 0; i < elements.length; i++) {
+    let e = elements[i];
+    console.log('element: ', e);
+
+    var src = "http://res.cloudinary.com/michelleerica/image/upload/v1501323957/"+ e.flourish.name +".png";
+
+    var flourish = fabric.Image.fromURL(src, function(showImg){
+      // debugger;
+      showImg.setLeft(e.left); console.log('e.left', e.left);
+      showImg.setTop(e.top);
+      showImg.setAngle(e.angle);
+      showImg.setScaleX(e.scaleX);
+      showImg.setScaleY(e.scaleY);
+      showImg.id = e.flourish.id;
+
+      canvas.add(showImg);
+      console.log('ON', i);
+
+    }, {crossOrigin: 'Anonymous'})
+
+  }
+
+} //edit only
 
 }); // end of document ready
--- app/assets/javascripts/showDesign.js
+++ app/assets/javascripts/showDesign.js
@@ -1,25 +1,9 @@
 
-var design_id = null;
-
 var canvas;
 
 $(document).ready(function(){
 
-
-if ($("body.designs.edit").length ||
-    $("body.designs.new"))
-    {
-  //---------------------design show page---------------------//
-  console.log('working??');
-
-    // debugger;
-
-    //rebuild existing design with element as saved in DB
-    //use elements details (e.g. img, width, height etc) hidden on design show page in flourish div and add to canvas
-  // var $imgElementShow =$('.flourish');
-  //
-  //   //flourish div created on show page as part of a loop. Number of flourish divs indicate how many elements there are to display
-
+  if ($("body.designs.edit").length){
   console.log(elements);
   console.log('length', elements.length);
 
@@ -45,85 +29,5 @@ if ($("body.designs.edit").length ||
 
   }
 
-
-  //------------ identify whats on canvas and update DB ------------ //
-
-  $("#updateDBButton").click(function(){
-
-    var elements = canvas.getObjects();
-    console.log(elements);
-
-    var elems = [];
-
-    for (var i = 80; i < elements.length; i++) {
-      var left = elements[i].get('left');
-      var top = elements[i].get('top');
-
-      var angle = elements[i].get('angle');
-      var scaleX = elements[i].get('scaleX');
-      var scaleY = elements[i].get('scaleY');
-      var flourish_id = elements[i].get('id');
-      console.log('scaleX', scaleX, 'scaleY', scaleY);
-      var elementInfo = {
-        left: left,
-        top: top,
-        angle: angle,
-        scaleX: scaleX,
-        scaleY: scaleY,
-        flourish_id: flourish_id
-      };
-
-      elems.push(elementInfo);
-    }
-    console.log(elems);
-    updateElementData(elems)
-
-  });
-
-  var updateElementData = function(info){
-    console.log('element info:', info);
-    // debugger;
-    var data = {
-      name: $('#designName').val(),
-      elements: info,
-      design_id:designData.id
-    };
-
-    //need to get design from params
-    var url = "";
-
-    if (data.design_id !== null) {
-      url = "/designs/"+data.design_id
-    } else {
-      url = design_id
-    };
-
-    // debugger;
-
-    $.ajax({
-      url: url,
-      data: data,
-      dataType: 'json',
-      method: 'PUT'
-    }).done(function(data){
-      // debugger;
-      console.log('DATA in ajax', data);
-      $('#saveStatus').text('DB save worked');
-
-      // save design_id in a global variable,
-      // and send it with all future requests
-      // to prevent a new design being created each
-      // time we save the elements
-      // design_id = data.id;
-
-    }).fail(function(xhr, err, status) {
-          console.log(xhr, err, status);
-    });
-
-  }
-
-
-
-}; //design#edit page
-
+  } //edit only
 }); // end of document ready
